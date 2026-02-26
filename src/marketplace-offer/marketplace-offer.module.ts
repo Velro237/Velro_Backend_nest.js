e1091e7b5c41ca6a97954b747b255e00298abd2e
Niloy Sarkar <dev.niloysarkar@gmail.com>
2026-02-26 18:41:31 +0600

    Imported Prisma module

--- src/marketplace-offer/marketplace-offer.module.ts
+++ src/marketplace-offer/marketplace-offer.module.ts
@@ -3,9 +3,10 @@ import { MarketplaceOfferService } from './marketplace-offer.service';
 import { MarketplaceOfferController } from './marketplace-offer.controller';
 import { MarketplaceListingModule } from 'src/marketplace-listing/marketplace-listing.module';
 import { ChatModule } from 'src/chat/chat.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [MarketplaceListingModule, ChatModule],
+  imports: [MarketplaceListingModule, ChatModule, PrismaModule],
   controllers: [MarketplaceOfferController],
   providers: [MarketplaceOfferService],
 })
